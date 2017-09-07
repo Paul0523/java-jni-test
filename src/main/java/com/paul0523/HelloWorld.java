07843f4d2f6fdb6507949d249f0f5df78e0d236b
fangzhipeng <750411463@qq.com>
2017-09-07 11:44:12 +0800

    Initial commit

---
+++ src/main/java/com/paul0523/HelloWorld.java
@@ -0,0 +1,45 @@
+package com.paul0523;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+public class HelloWorld{
+    public native void hello();
+
+    static {
+        try {
+            //设置查找路径为当前项目路径,注意直接设置java.library.path属性无效
+            addDir("/Users/fangzhipeng/Documents/workspace-idea/java-jni-test/lib");
+            //加载动态库的名称
+            System.loadLibrary("hello");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args){
+        new HelloWorld().hello();
+    }
+
+    public static void addDir(String s) throws IOException {
+        try {
+            Field field = ClassLoader.class.getDeclaredField("usr_paths");
+            field.setAccessible(true);
+            String[] paths = (String[])field.get(null);
+            for (int i = 0; i < paths.length; i++) {
+                if (s.equals(paths[i])) {
+                    return;
+                }
+            }
+            String[] tmp = new String[paths.length+1];
+            System.arraycopy(paths,0,tmp,0,paths.length);
+            tmp[paths.length] = s;
+            field.set(null,tmp);
+        } catch (IllegalAccessException e) {
+            throw new IOException("Failed to get permissions to set library path");
+        } catch (NoSuchFieldException e) {
+            throw new IOException("Failed to get field handle to set library path");
+        }
+    }
+}
